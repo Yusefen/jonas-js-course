7bd9f074b7d3d309d752f377df5001868d67d2fb
asalentezari <asal@asalentezaris-MacBook-Pro.local>
2025-02-12 21:13:52 +0330

    first

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,64 @@
+/*
+let js = "amazing";
+console.log(10 - 23 + 10 + 5);
+console.log("yusef");
+console.log(23);
+let firstname = "yusef";
+console.log(firstname);
+let javascriptisfun = true;
+console.log(javascriptisfun);
+console.log(typeof true);
+console.log(typeof javascriptisfun);
+console.log(typeof 'yusef');
+console.log(typeof 23);
+javascriptisfun = 'yes';
+console.log(typeof javascriptisfun);
+let year;
+console.log(year);
+console.log(typeof year);
+year = 1999;
+console.log(typeof year);
+console.log(typeof null);
+*/
+/*
+let age = 25;
+age = 26;
+console.log(age);
+lastname = 'en';
+console.log(lastname);
+*/
+/*
+const now = 2034
+const ageofyusef = now - 1998;
+const ageofsahel = now - 1969;
+console.log(ageofyusef, ageofsahel);
+console.log(ageofyusef * 2, ageofyusef / 10, 2 ** 3);
+const firstname = 'yusef';
+const lastname = 'en';
+console.log(firstname + ' ' + lastname);
+let x = 10 + 5;
+x += 10;//x=x+10
+x *= 4;//x=x*4
+x++;
+x--;
+x--;
+
+
+console.log(x);
+console.log(ageofyusef > ageofsahel);
+console.log(ageofsahel >= 18);
+const isfullage = ageofsahel >= 18;
+console.log(now - 1998 < now - 1969);
+*/
+/*
+const now = 2034
+const ageofyusef = now - 1998;
+const ageofsahel = now - 1969;
+console.log(now - 1998 < now - 1969);
+
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y);
+const averageage = (ageofsahel + ageofyusef) / 2;
+console.log(ageofsahel, ageofyusef, averageage);
+*/
